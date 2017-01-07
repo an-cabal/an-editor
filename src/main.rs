d9e0cb468492aa5f7f997add87bf2cdc1f58bda7
Eliza Weisman <hi@hawkweisman.me>
2017-01-07 12:03:54 -0500

    chore(clippy): add support for linting w/ clippy

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-#![cfg_attr(feature = "unstable", feature(try_from))]
-
 //! An editor
+
+#![cfg_attr(feature = "unstable", feature(try_from))]
+#![cfg_attr( feature = "clippy", feature(plugin) )]
+#![cfg_attr( feature = "clippy", plugin(clippy) )]
 
 extern crate an_rope;
 
