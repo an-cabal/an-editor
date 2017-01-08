b06eb4dafa4b6708bdd403a91ec5c23137d01eab
Eliza Weisman <hi@hawkweisman.me>
2017-01-08 13:03:55 -0500

    feat(edit history) start on edit history functions

--- src/history.rs
+++ src/history.rs
@@ -3,4 +3,29 @@
 use an_zipper::ZipList;
 use an_rope::Rope;
 
-pub type Stack = ZipList<Rope>;
+#[derive(Debug)]
+pub struct Stack(ZipList<Rope>);
+
+impl Stack {
+    /// Construct a new history stack
+    pub fn new() -> Self {
+        Stack(ZipList::new())
+    }
+
+    pub fn undo(&mut self) {
+        self.0.move_left();
+    }
+
+    pub fn redo(&mut self) {
+        self.0.move_right();
+    }
+
+    pub fn edit<F>(&mut self, f: F)
+    where F: Fn(&Rope) -> Rope {
+        let state = {
+            let curr = self.0.peek_left();
+            f(curr.unwrap_or( &Rope::new() ))
+        };
+        self.0.push_left(state);
+    }
+}
