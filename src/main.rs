fd33acf563ac35913041e7851808323395327dac
Eliza Weisman <hi@hawkweisman.me>
2017-02-10 10:41:10 -0500

    feat(app): add CLI option for reading a config file

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ extern crate clap;
 pub mod buffer;
 pub mod history;
 
+/// Tests whether or not a file actually exists
 fn file_exists(path: String) -> Result<(), String> {
     if std::fs::metadata(path.clone()).is_ok() {
         Ok(())
@@ -33,7 +34,11 @@ fn main() {
     ).get_matches();
 
     if let Some(cfg) = args.value_of("config") {
-        // load custom config file, otherwise load ~/.anrc
-        unimplemented!()
+        // FIXME: unimplemented
+        unimplemented!();
+    } else if let Ok(_) = file_exists(String::from("~/.anrc")) {
+        // FIXME: do we want "~/.anrc" to be the name of the config file?
+        // FIXME: unimplemented
+        unimplemented!();
     }
 }
