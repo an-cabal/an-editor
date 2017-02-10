4f0dbe513ae01e63be42a0213d2b74a608a66d39
Eliza Weisman <hi@hawkweisman.me>
2017-02-10 10:29:50 -0500

    feat(app): quick first pass on command-line argument parsing

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,18 @@
 
 extern crate an_rope;
 extern crate an_zipper;
+#[macro_use]
+extern crate clap;
 
 pub mod buffer;
 pub mod history;
 
 fn main() {
-
+    let args = clap_app!(an =>
+        (version: crate_version!())
+        (author: crate_authors!(", "))
+        (about: "An text editor.")
+            (@arg INPUT: +required "Sets the input file to open.")
+            (@arg debug: -d ... "Sets the level of debugging information")
+    ).get_matches();
 }
